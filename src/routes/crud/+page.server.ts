98e0d813cbbd4eae9e0e7f96ce525bf74514311b
k-ishida <60775539+ki412412@users.noreply.github.com>
2023-10-29 04:30:44 +0900

    feat(crud): Delete old avatar file after one hour

--- src/routes/crud/+page.server.ts
+++ src/routes/crud/+page.server.ts
@@ -1,7 +1,35 @@
 import type { PageServerLoad } from './$types';
 import { users } from '$lib/users';
+import Fs from 'fs';
+
+
+function createdDate(file: Fs.PathLike) {  
+    const { birthtime } = Fs.statSync(file)
+  
+    return birthtime
+}
+
+function deleteOldFiles(dir: Fs.PathLike) {
+    const now = Date.now();
+    const files = Fs.readdirSync(dir);
+    for (const file of files) {
+        const filePath = `${dir}/${file}`;
+        const created = createdDate(filePath);
+        const oneHour = 60 * 60 * 1000; // 1 hour in milliseconds
+        
+        if (file.startsWith('favicon')) {
+            continue;
+        }
+        if (now - created > oneHour) {
+            Fs.unlinkSync(filePath);
+        }
+    }
+}
 
 export const load = (async () => {
+
+    deleteOldFiles('static');
+    
     return {
         users: users
     };
