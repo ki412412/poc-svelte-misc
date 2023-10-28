67511bffd31c636e70c0264ed97c29af28500c6e
k-ishida <60775539+ki412412@users.noreply.github.com>
2023-10-29 03:11:00 +0900

    feat(crud): Add photo preview function

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,10 +1,27 @@
 import { z } from 'zod';
 
+const MAX_FILE_SIZE = 1024 * 1024 * 5;
+const ACCEPTED_IMAGE_MIME_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
 // See https://zod.dev/?id=primitives for schema syntax
 export const userSchema = z.object({
     id: z.string().regex(/^\d+$/),
     name: z.string().min(2),
-    email: z.string().email()
+    email: z.string().email(),
+    avatar: z.custom<FileList>()
+            .refine((file) => file.length !== 1, { message: 'Required.' })
+            .transform((file) => file[0])
+            .refine((file) => file.size <= MAX_FILE_SIZE, { message: 'Max image size is 5MB.' })
+            .refine(
+                (file) => ACCEPTED_IMAGE_MIME_TYPES.includes(file?.type),
+                "Only .jpg, .jpeg, .png and .webp formats are supported."
+            )
+            .optional().nullable()
 });
 
 type UserDB = z.infer<typeof userSchema>[];
@@ -17,16 +34,19 @@ export const users: UserDB = [
     {
         id: userId(),
         name: '🙋‍♂️David Davis',
-        email: 'david@example.com'
+        email: 'david@example.com',
+        photo: null 
     },
     {
         id: userId(),
         name: '🙋‍♀️Rachel Ray',
-        email: 'Rrachel@example.com'
+        email: 'Rrachel@example.com',
+        photo: null,
     },
     {
         id: userId(),
         name: '🙋George Michel',
-        email: 'george@example.com'
+        email: 'george@example.com',
+        photo: null,
     }
 ];
--- src/routes/crud/[id]/+page.svelte
+++ src/routes/crud/[id]/+page.svelte
@@ -8,6 +8,18 @@
 	export let data: PageData;
 
 	const { form, errors, constraints, enhance, delayed, message } = superForm(data.form);
+
+    let photoPreview: string | ArrayBuffer | null;
+    let fileinput; // file input element
+
+    function previewPhoto(e: Event) {
+        const image = e.target?.files[0];
+        let reader = new FileReader();
+        reader.readAsDataURL(image);
+        reader.onload = e => {
+            photoPreview = e.target.result
+        };
+    }
 </script>
 
 <p class="mt-10 text-center text-xl">--- {!$form.id ? 'Create' : 'Update'} ---</p>
@@ -17,9 +29,34 @@
         <h3 class="text-center text-lime-400" class:!text-red-600={$page.status >= 400} >{$message}</h3>
     {/if}
 
-    <form method="POST" use:enhance class="flex flex-col gap-6">
+    <form method="POST" use:enhance class="flex flex-col gap-6 ">
         <input type="hidden" name="id" bind:value={$form.id} />
-    
+        <div>
+            <!-- Avatar -->
+            <div>
+                <div class="flex items-center justify-center overflow-hidden">
+                    <div class="overflow-hidden">
+                        {#if photoPreview}
+                            <img class=" w-52 aspect-square rounded-full overflow-hidden" src={photoPreview} alt="Avatar" />
+                        {:else}
+                            <figure>
+                                <svg class="w-52 h-52"xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>                                  
+                            </figure>
+                        {/if}
+                    </div>
+                    <button type="button" on:click={()=>{fileinput.click()}} class="self-end cursor-pointer -ml-5 hover:text-gray-200">
+                        <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <input class="hidden" type="file" accept=".jpg, .jpeg, .png" on:change={(e)=>previewPhoto(e)} bind:this={fileinput} >
+            {#if $errors.avatar}<span class="text-red-600">{$errors.avatar}</span>{/if}
+        </div>
         <div class="flex flex-col">
             <label for="name">Name</label>
             <input
