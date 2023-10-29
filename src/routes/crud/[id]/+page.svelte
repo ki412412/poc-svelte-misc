7d857fdb636affcae0412a0ca61acb098847175a
k-ishida <60775539+ki412412@users.noreply.github.com>
2023-10-30 01:01:38 +0900

    refactor(crud): Refactor avatar selection

--- src/routes/crud/[id]/+page.svelte
+++ src/routes/crud/[id]/+page.svelte
@@ -10,7 +10,6 @@
 	const { form, errors, constraints, enhance, delayed, message } = superForm(data.form);
 
     let photoPreview: string | ArrayBuffer | null;
-    let fileinput; // file input element
 
     function previewPhoto(e: Event) {
         const image = e.target?.files[0];
@@ -35,7 +34,7 @@
             <!-- Avatar -->
             <div>
                 <div class="flex items-center justify-center overflow-hidden">
-                    <div class="overflow-hidden">
+                    <label for="avatar" class="overflow-hidden hover:cursor-pointer">
                         {#if photoPreview}
                             <img class=" w-52 aspect-square rounded-full overflow-hidden" src={photoPreview} alt="Avatar" />
                         {:else}
@@ -49,16 +48,17 @@
                             </figure>
                             {/if}
                         {/if}
-                    </div>
-                    <button type="button" on:click={()=>{fileinput.click()}} class="self-end cursor-pointer -ml-5 hover:text-gray-200">
+                        <!-- edit icon -->
+                    </label>
+                    <label for="avatar" class="self-end cursor-pointer -ml-5 hover:text-gray-200">
                         <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
                         </svg>
-                    </button>
+                    </label>
                 </div>
             </div>
-            <input class="hidden" type="file" name="avatar" accept=".jpg, .jpeg, .png" on:change={(e)=>previewPhoto(e)} bind:this={fileinput} >
+            <input class="hidden" type="file" id="avatar" name="avatar" accept=".jpg, .jpeg, .png" on:change={(e)=>previewPhoto(e)} />
             {#if $errors.avatar}<span class="text-red-600">{$errors.avatar}</span>{/if}
         </div>
         <div class="flex flex-col">
